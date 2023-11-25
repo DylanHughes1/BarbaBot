ed16d0dea6b72fa35aadea8c4fb59a55519bde5d
Dylan Hughes <dylan_21174@hotmail.com>
2023-11-25 16:29:12 -0300

    Update github url

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ client.on('messageCreate', async (message) => {
     const exampleEmbed = new EmbedBuilder()
       .setColor(0x0099FF)
       .setTitle('BabarBot')
-      .setURL('https://github.com/DylanHughes1')
+      .setURL('https://github.com/DylanHughes1/BarbaBot')
       .setAuthor({ name: 'Dylan Hughes' })
       .setDescription('Bienvenidos al bot de Barbakahn. Para reproducir un sonido, escriba "!play" y el nombre del sonido. Para ver la lista de sonidos, escriba "!sounds". ')
       .setThumbnail('https://pm1.aminoapps.com/6426/ff34f4317d56bd4448a57c95db21b5718d7663dd_00.jpg')
