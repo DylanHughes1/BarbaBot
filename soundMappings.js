3d460e172bee6b13446cda0731be18acfbf57248
DylanHughes1 <71235743+DylanHughes1@users.noreply.github.com>
2023-12-02 22:53:53 +0000

    sounds added

--- soundMappings.js
+++ soundMappings.js
@@ -1,13 +1,16 @@
 const soundMappings = {
-    'ahhizi': './sounds/ahhizi.mp3',
-    'quetal': './sounds/quetal.mp3',
-    'cuandoVemos': './sounds/cuandoVemos.mp3',
-    'daleVoli': './sounds/daleVoli.mp3',
-    'oneDay': './sounds/oneDay.mp3',
-    'pro': './sounds/pro.mp3',
-    'loMismo': './sounds/loMismo.mp3',
-    'freeKill': './sounds/freeKill.mp3',
-  };
-  
-  module.exports = soundMappings;
-  
+  'ahhizi': './sounds/ahhIzi.mp3',
+  'quetal': './sounds/quetal.mp3',
+  'cuandoVemos': './sounds/cuandoVemos.mp3',
+  'daleVoli': './sounds/daleVoli.mp3',
+  'oneDay': './sounds/oneDay.mp3',
+  'pro': './sounds/pro.mp3',
+  'loMismo': './sounds/loMismo.mp3',
+  'freeKill': './sounds/freeKill.mp3',
+  'pete': './sounds/pete.mp3',
+  'tutorial': './sounds/tutorial.mp3',
+  'patrulla': './sounds/patrulla.mp3',
+  'zafo': './sounds/zafo.mp3',
+};
+
+module.exports = soundMappings;
