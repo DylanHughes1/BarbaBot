7bc531e750becb5b87f41f31aa4825bc0446ba42
DylanHughes1 <71235743+DylanHughes1@users.noreply.github.com>
2023-12-02 22:28:52 +0000

    Libs updated

--- commands/play.js
+++ commands/play.js
@@ -2,48 +2,48 @@ const { createAudioResource, joinVoiceChannel, AudioPlayerStatus } = require('@d
 
 
 function playCommand(message, queues, soundMappings, audioPlayer) {
-  
-    const voiceChannel = message.member.voice.channel;
 
-    if (voiceChannel) {
-      const connection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-      });
+  const voiceChannel = message.member.voice.channel;
 
-      const command = message.content.split(' ')[1];
+  if (voiceChannel) {
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
 
-      if (soundMappings[command]) {
-        const soundPath = soundMappings[command];
+    const command = message.content.split(' ')[1];
 
-        let queue = queues.get(message.guild.id);
+    if (soundMappings[command]) {
+      const soundPath = soundMappings[command];
 
-        if (!queue) {
-          queue = [];
-          queues.set(message.guild.id, queue);
+      let queue = queues.get(message.guild.id);
 
-          audioPlayer.on(AudioPlayerStatus.Idle, () => {
-            const nextSound = queue.shift();
-            if (nextSound) {
-              audioPlayer.play(nextSound);
-            } 
-          });
-        }
+      if (!queue) {
+        queue = [];
+        queues.set(message.guild.id, queue);
 
-        const audioResource = createAudioResource(soundPath);
-        queue.push(audioResource);
+        audioPlayer.on(AudioPlayerStatus.Idle, () => {
+          const nextSound = queue.shift();
+          if (nextSound) {
+            audioPlayer.play(nextSound);
+          }
+        });
+      }
 
-        if (audioPlayer.state.status !== AudioPlayerStatus.Playing) {
-          audioPlayer.play(queue.shift());
-          connection.subscribe(audioPlayer);
-        }
-      } else {
-        message.reply('Cuando vemos que no es ninguna novedad que escriban mal el comando');
+      const audioResource = createAudioResource(soundPath);
+      queue.push(audioResource);
+
+      if (audioPlayer.state.status !== AudioPlayerStatus.Playing) {
+        audioPlayer.play(queue.shift());
+        connection.subscribe(audioPlayer);
       }
     } else {
-      message.reply('Debes estar en un canal de voz para usar este comando');
+      message.reply('Cuando vemos que no es ninguna novedad que escriban mal el comando');
     }
+  } else {
+    message.reply('Debes estar en un canal de voz para usar este comando');
+  }
 }
 
 module.exports = playCommand;
